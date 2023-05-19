469842fd5243ccb23cc48cfa008558239327c525
marochkatamarochka <tomakss10@gmail.com>
2023-05-19 10:09:59 +0300

    week-4

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,7 +1,10 @@
-// eslint-disable-next-line no-unused-vars
+
 import photosDB from './photos.json';
-// eslint-disable-next-line no-unused-vars
 import friendsDB from './friends.json';
+const PERM_FRIENDS = 2;
+const PERM_PHOTOS = 4;
+const APP_ID = 5350105;
+
 
 export default {
   getRandomElement(array) {
@@ -12,12 +15,89 @@ export default {
   return array[index];
   },
 
-  getNextPhoto() {
-    const friend = this.getRandomElement(friendsDB);
-    const photos = photosDB[friend.id];
-    const photo = this.getRandomElement(photos);
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await.this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos, items);
+    const size = this.findSize(photo);
 
-    return { friend, url: photo.url };
+    return { friend, id: photo.id, url: size.url };
   },
 
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+        return biggest;
+       } photo.sizes[0]);
+    }
+    return size;
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
+login() {
+  return new Promise((resolve, reject) => {
+    VideoPlaybackQuality.init({
+      aplid: APP_ID,
+    });
+
+    VideoPlaybackQuality.Auth.login((Response) => {
+      if (Response.session) {
+        resolve(Response);
+      } else {
+        console.error(Response);
+        reject(Response);
+      }
+    }, PERM_FRIENDS | PERM_PHOTOS);
+  });
+},
+
+callApi(method, params) {
+  params.v = params.v || '5.120';
+
+  return new Promise((resolve, reject) => {
+    VideoPlaybackQuality.api(method, params, (Response) => {
+      if (Response.error) {
+        reject(new Error(Response.error.error_msg));
+      } else {
+        resolve(Response.Response);
+      }
+    });
+  });
+},
+
+  getFriends() {
+    const params = {
+      friends: ['photo_50', 'photo_100'],
+    };
+    return this.callApi('friends.get', params);
+  },
+
+getPhotos(owner) {
+  const params = {
+    owner_id: owner,
+  };
+  
+  return this.callApi('photos.getAll', params);
+},
+
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+    photos = await this.getFriendPhotos(id);
+    this.photoCache[id] = photos;
+
+    return photos
+  },
 };
